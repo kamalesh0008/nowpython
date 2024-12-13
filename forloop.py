3242d0d77234f6387df5c2bb46c34b75f9336e5e
kamalesh0008 <kamaleshk9978@gmail.com>
2024-12-13 09:18:47 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,25 @@
+for x in "1234":
+    print(x)
+
+#range(stop)
+for i in range(7):
+    print(i)
+
+#range(start,stop)
+for i in range(50,54):
+    print(i)
+
+#range(start,stop,step)
+for i in range(50,100,2):
+    print(i)
+
+#if else
+for i in range(10):
+    if i%2==0:
+        print(i,"even")
+    else:
+        print(i,"odd")
+
+
+
+
